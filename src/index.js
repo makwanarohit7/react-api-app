6d6cb550a362824c02e7777a5cc670431d64730e
Rohit <makawanarohit70@gmail.com>
2022-11-28 18:37:36 +0530

    thard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./gender_api/App";
+import App from "./Data/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
